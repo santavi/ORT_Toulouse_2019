7e7b4d56893f3bfe71ca1bd985dc6f2825e63274
santavi <37830708+santavi@users.noreply.github.com>
2019-12-31 14:28:20 +0100

    Add files via upload

---
+++ Devoir_html_css_js/js/bouton_remonte_haut_page.js
@@ -0,0 +1,16 @@
+jQuery(document).ready(function() {
+    var duration = 500;
+    jQuery(window).scroll(function() {
+      if (jQuery(this).scrollTop() > 100) {
+        jQuery('.cRetour').fadeIn(duration);
+      } else {
+        jQuery('.cRetour').fadeOut(duration);
+      }
+    });
+          
+    jQuery('.cRetour').click(function(event) {
+      event.preventDefault();
+      jQuery('html, body').animate({scrollTop: 0}, duration);
+      return false;
+    })
+  });
